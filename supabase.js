e18dc923bbfbbb5dfd19871bd2d81f356b1e6dba
Qriyou <sanadsyed2@gmail.com>
2026-08-16 11:13:43 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL =
+    "https://ovfyoseraxfwdglykazb.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+    "sb_publishable_eyIgn1e7w_AufYf4Y4jGdg_8T3WEvXL";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
