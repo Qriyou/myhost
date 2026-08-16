971b07f367a64a5e762dd6bfc776242f10ed05e4
Qriyou <sanadsyed2@gmail.com>
2026-08-16 11:04:54 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+function signup() {
+    alert("Signup will be connected to Supabase soon!");
+}
+
+function login() {
+    alert("Login will be connected to Supabase soon!");
+}
+
+function learnMore() {
+    document.querySelector(".features").scrollIntoView({
+        behavior: "smooth"
+    });
+}
